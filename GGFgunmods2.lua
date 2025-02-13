9a443377d2c8b071507c29542afd225408a143d0
xeonise <ghkkq@outlook.com>
2025-02-13 17:17:20 +0200

    even better way

--- GGFgunmods2.lua
+++ GGFgunmods2.lua
@@ -1,15 +1,22 @@
 -- variant 2 for shit sploits like solara/xeno, etc. ; also require exists in xeno, but when I tried to change a function with it, it gave me a error, due to the level of the executor being low.
 while true do task.wait()
-    local item = workspace:FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChildOfClass("Tool")
+    local player = game.Players.LocalPlayer
+    local item = workspace:FindFirstChild(player.Name):FindFirstChildOfClass("Tool")
     if item then
-        if item.Configuration.reloadTime then 
-            item.Configuration.reloadTime.Value = 0
-        elseif item.Configuration.FireRate then 
-            item.Configuration.FireRate.Value = 0
-        elseif item.Configuration.recoil then
-            item.Configuration.recoil.Value = 0
-        elseif item.Configuration.isAuto then
-            item.Configuration.isAuto.Value = true
-        end 
+        local config = item:FindFirstChild("Configuration")
+        if config then
+            if config:FindFirstChild("reloadTime") then 
+                config.reloadTime.Value = 0
+            end
+            if config:FindFirstChild("FireRate") then 
+                config.FireRate.Value = 0
+            end
+            if config:FindFirstChild("recoil") then
+                config.recoil.Value = 0
+            end
+            if config:FindFirstChild("isAuto") then
+                config.isAuto.Value = true
+            end 
+        end
     end
 end
