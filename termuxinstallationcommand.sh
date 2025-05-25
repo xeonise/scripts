d87d6c397dd06d623d3cea753e0f5a9a8ca51aec
xeonise <ghkkq@outlook.com>
2025-05-25 11:50:01 +0300

    Update termuxinstallationcommand.sh

--- termuxinstallationcommand.sh
+++ termuxinstallationcommand.sh
@@ -1,1 +1,1 @@
-apt-get install -y python3 wget && wget -q https://raw.githubusercontent.com/xeonise/scripts/refs/heads/main/reconnectserver.py -O reconnectserver.py && python3 reconnectserver.py 
+apt-get install -y python3 wget && pip install websockets && wget -q https://raw.githubusercontent.com/xeonise/scripts/refs/heads/main/reconnectserver.py -O reconnectserver.py && python3 reconnectserver.py 
